0d91ac671a2976f885e48bbe097025f6b3a75f1c
bibibi-Matrix <nikolay.peschansky@gmail.com>
2026-09-05 15:04:37 +0300

    luci-app-bird v1.0.1: replace removed ui.createWidget with E() DOM in status/sync/lists views (LuCI 26 compat)

--- luci-app-bird/htdocs/luci-static/resources/view/bird/status.js
+++ luci-app-bird/htdocs/luci-static/resources/view/bird/status.js
@@ -26,65 +26,69 @@ return view.extend({
 		return callBird().then(function(data) {
 			var container = E([]);
 
-			var statusBox = ui.createWidget(null, { label: _('Service status') });
-			statusBox.node.appendChild(
-				E('div', { 'class': 'cbi-value' },
+			var statusChildren = [
+				E('h3', { 'class': 'cbi-section-title' }, _('Service status')),
+				E('div', { 'class': 'cbi-value' }, [
 					E('label', { 'class': 'cbi-value-title' }, _('Service enabled')),
 					E('div', { 'class': 'cbi-value-field' },
-						E('span', { 'class': data.enabled ? 'fa fa-check' : 'fa fa-times' }, data.enabled ? _('Enabled') : _('Disabled')))));
-
-			statusBox.node.appendChild(
-				E('div', { 'class': 'cbi-value' },
+						E('span', { 'class': data.enabled ? 'fa fa-check' : 'fa fa-times' }, data.enabled ? _('Enabled') : _('Disabled')))
+				]),
+				E('div', { 'class': 'cbi-value' }, [
 					E('label', { 'class': 'cbi-value-title' }, _('Daemon running')),
 					E('div', { 'class': 'cbi-value-field' },
-						E('span', { 'class': data.running ? 'fa fa-check' : 'fa fa-times' }, data.running ? _('Running') : _('Stopped')))));
+						E('span', { 'class': data.running ? 'fa fa-check' : 'fa fa-times' }, data.running ? _('Running') : _('Stopped')))
+				])
+			];
 
 			if (data.uptime)
-				statusBox.node.appendChild(
-					E('div', { 'class': 'cbi-value' },
+				statusChildren.push(
+					E('div', { 'class': 'cbi-value' }, [
 						E('label', { 'class': 'cbi-value-title' }, _('Uptime')),
-						E('div', { 'class': 'cbi-value-field' }, E('textarea', { 'readonly': 'readonly', 'rows': 1 }, data.uptime))));
+						E('div', { 'class': 'cbi-value-field' }, E('textarea', { 'readonly': 'readonly', 'rows': 1 }, data.uptime))
+					]));
 
 			if (data.lastSync)
-				statusBox.node.appendChild(
-					E('div', { 'class': 'cbi-value' },
+				statusChildren.push(
+					E('div', { 'class': 'cbi-value' }, [
 						E('label', { 'class': 'cbi-value-title' }, _('Last sync')),
-						E('div', { 'class': 'cbi-value-field' }, E('textarea', { 'readonly': 'readonly', 'rows': 1 }, data.lastSync))));
+						E('div', { 'class': 'cbi-value-field' }, E('textarea', { 'readonly': 'readonly', 'rows': 1 }, data.lastSync))
+					]));
 
-			container.appendChild(statusBox.node);
+			container.appendChild(E('div', { 'class': 'cbi-section' }, statusChildren));
 
-			if (data.protocols) {
-				var protoBox = ui.createWidget(null, { label: _('BIRD protocols') });
-				protoBox.node.appendChild(
-					E('pre', { 'class': 'cbi-section-node' }, data.protocols));
-				container.appendChild(protoBox.node);
-			}
+			if (data.protocols)
+				container.appendChild(
+					E('div', { 'class': 'cbi-section' }, [
+						E('h3', { 'class': 'cbi-section-title' }, _('BIRD protocols')),
+						E('pre', { 'class': 'cbi-section-node' }, data.protocols)
+					]));
 
-			var actionBox = ui.createWidget(null, { label: _('Actions') });
-			actionBox.node.appendChild(
-				E('div', { 'class': 'cbi-page-actions' },
-					E('input', {
-						'type': 'button',
-						'class': 'cbi-button cbi-button-apply',
-						'value': _('Reload Bird configuration'),
-						'click': function() {
-							callConfigure().then(function() {
-								ui.addNotification(null, E('p', _('Bird configuration reloaded')));
-							});
-						}
-					}),
-					E('input', {
-						'type': 'button',
-						'class': 'cbi-button cbi-button-reset',
-						'value': _('Run list sync'),
-						'click': function() {
-							callSync().then(function() {
-								ui.addNotification(null, E('p', _('List sync finished')));
-							});
-						}
-					})));
-
-			container.appendChild(actionBox.node);
+			container.appendChild(
+				E('div', { 'class': 'cbi-section' }, [
+					E('h3', { 'class': 'cbi-section-title' }, _('Actions')),
+					E('div', { 'class': 'cbi-page-actions' }, [
+						E('input', {
+							'type': 'button',
+							'class': 'cbi-button cbi-button-apply',
+							'value': _('Reload Bird configuration'),
+							'click': function() {
+								callConfigure().then(function() {
+									ui.addNotification(null, E('p', _('Bird configuration reloaded')));
+								});
+							}
+						}),
+						E('input', {
+							'type': 'button',
+							'class': 'cbi-button cbi-button-reset',
+							'value': _('Run list sync'),
+							'click': function() {
+								callSync().then(function() {
+									ui.addNotification(null, E('p', _('List sync finished')));
+								});
+							}
+						})
+					])
+				]));
 
 			return container;
 		});
